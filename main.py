d30d64fe869e3caf95263ce33f68c5ffe288d427
RAMAN RANJAN DASH <71960208+RrD-111@users.noreply.github.com>
2024-08-27 10:07:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
 from src.routers import auth, users, groups, messages
 
+"""
+Group Chat API
+
+A FastAPI-based Group Chat API that allows users to create accounts, join groups, send messages, and like messages within groups.
+
+Routers:
+- auth: Authentication routes for user login and token generation.
+- users: User-related routes for creating and updating user accounts.
+- groups: Group-related routes for creating, deleting, listing groups, and adding members to groups.
+- messages: Message-related routes for sending messages to groups and liking messages.
+
+Usage:
+- Run the API server using `uvicorn main:app --reload`.
+- Access the API documentation at `http://localhost:8000/docs` or `http://localhost:8000/redoc`.
+"""
 app = FastAPI(title="Group Chat API",
     description="A FastAPI-based Group Chat API that allows users to create accounts, join groups, send messages, and like messages within groups.",
     version="1.0.0",)
