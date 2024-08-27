3e6db17dd34eb7bba2e2a52a0bc894309d83891a
RAMAN RANJAN DASH <71960208+RrD-111@users.noreply.github.com>
2024-08-27 09:48:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from src.routers import auth, users, groups, messages
 
-app = FastAPI()
+app = FastAPI(title="Group Chat API",
+    description="A FastAPI-based Group Chat API that allows users to create accounts, join groups, send messages, and like messages within groups.",
+    version="1.0.0",)
 
 app.include_router(auth.router)
 app.include_router(users.router)
