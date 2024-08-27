10420e25e20af80618b7730a37ed498f19f505ee
RAMAN RANJAN DASH <71960208+RrD-111@users.noreply.github.com>
2024-08-27 09:52:20 +0530

    Update db_util.py

--- src/utils/db_util.py
+++ src/utils/db_util.py
@@ -5,6 +5,12 @@ import psycopg2
 load_dotenv()
 
 def get_db_connection():
+     """
+    Create and return a connection to the database.
+
+    Returns:
+        psycopg2.extensions.connection: A connection to the PostgreSQL database.
+    """
     conn = psycopg2.connect(
         host=os.getenv('DB_HOST'),
         database=os.getenv('DB_NAME'),
